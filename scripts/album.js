74a4046de86df98b4e206f1b226b5a377dc159c4
Kristin Gritt <kristin.gritt@gmail.com>
2017-03-12 21:45:46 -0500

    update album.js file

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@ var ablumMarconi = {
 	]
 };
 
+// Adding a third album for assignment 11
+var ablumKodiak = {
+	title: 'The Bear Hunter',
+	artist: 'Tyler Haines',
+	label: 'Star',
+	year: '1983',
+	albumArtUrl: 'assets/images/album_covers/13.png',
+	songs: [
+		{ title: 'High Seas', duration: '1:52' },
+		{ title: 'Climbing Mountains', duration: '3:01' },
+		{ title: 'Harbor Master', duration: '2:51' },
+		{ title: 'The Wind Always Blows', duration: '2:15' },
+		{ title: 'Bear It All', duration: '4:15' }
+	]
+};
+
 // create a function named createSongRow that generates the song row content - we declare the objects before the function because the createSongRow function uses the info stored in the album objects
 
 var createSongRow = function(songNumber, songName, songLength) { // assigns our previously static song row template to a variable named template and returns it
@@ -47,13 +63,7 @@ var createSongRow = function(songNumber, songName, songLength) { // assigns our 
 // create a function that is called when window loads and takes one of our album objects as an arguement and will utilize the object's stored info by injecting it into the template
 
 var setCurrentAlbum = function(album) {
-	// #1 select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list. 
-	var albumTitle = document.getElementsByClassName('album-view-title')[0];
-	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-	var albumImage = document.getElementsByClassName('album-cover-art')[0];
-	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-	
+
 	// #2 the firstChild property identifies the first child node of an element, and nodeValue returns or sets the value of a node.
 	albumTitle.firstChild.nodeValue = album.title;
 	albumArtist.firstChild.nodeValue = album.artist;
@@ -71,4 +81,15 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
+
+// add event listener to album cover, when user clicks, toggles betweent album objects
+	var albums = [albumPicasso, albumMarconi, albumKodiak];
+	var index = 1;
+	albumImage.addEventListener("click", function(event) {
+		setCurrentAlbum(albums[index]);
+		index++;
+		if (index == albums.length) {
+			index = 0;
+		}
+	});
 };
