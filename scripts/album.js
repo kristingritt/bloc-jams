03bf59da1ce8bdc98c1725b9fb7427a7d8dcd536
Kristin Gritt <kristin.gritt@gmail.com>
2017-03-19 20:43:42 -0500

    completes assignment 11 and adds event click

--- scripts/album.js
+++ scripts/album.js
@@ -5,9 +5,9 @@ var albumPicasso = {
 	label: 'Cubism',
 	year: '1881',
 	albumArtUrl: 'assets/images/album_covers/01.png',
-	song: [
+	songs: [
 		{ title: 'Blue', duration: '4:26' },
-		{ title: 'Green', duraiton: '3:14' },
+		{ title: 'Green', duration: '3:14' },
 		{ title: 'Red', duration: '5:01' },
 		{ title: 'Pink', duration: '3:21' },
 		{ title: 'Magenta', duration: '2:15' }
@@ -15,7 +15,7 @@ var albumPicasso = {
 };
 
 // Another Example Album
-var ablumMarconi = {
+var albumMarconi = {
 	title: 'The Telephone',
 	artist: 'Guglielmo Marconi',
 	label: 'EM',
@@ -31,7 +31,7 @@ var ablumMarconi = {
 };
 
 // Adding a third album for assignment 11
-var ablumKodiak = {
+var albumKodiak = {
 	title: 'The Bear Hunter',
 	artist: 'Tyler Haines',
 	label: 'Star',
@@ -50,7 +50,7 @@ var ablumKodiak = {
 
 var createSongRow = function(songNumber, songName, songLength) { // assigns our previously static song row template to a variable named template and returns it
 	var template = 
-		'<tr class="album-view=song-item">'
+		'<tr class="album-view-song-item">'
 	  +	'	<td class="song-item-number">' + songNumber + '</td>'
 	  +	'	<td class="song-item-title">' + songName + '</td>'
 	  +	'	<td class="song-item-duration">' + songLength + '</td>'
@@ -62,10 +62,16 @@ var createSongRow = function(songNumber, songName, songLength) { // assigns our 
 
 // create a function that is called when window loads and takes one of our album objects as an arguement and will utilize the object's stored info by injecting it into the template
 
-var setCurrentAlbum = function(album) {
+// set elements to populate text
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+var setCurrentAlbum = function(album) {
 	// #2 the firstChild property identifies the first child node of an element, and nodeValue returns or sets the value of a node.
-	albumTitle.firstChild.nodeValue = album.title;
+	albumTitle.firstChild.nodeValue = album.name;
 	albumArtist.firstChild.nodeValue = album.artist;
 	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
 	albumImage.setAttribute('src', album.albumArtUrl);
@@ -74,21 +80,20 @@ var setCurrentAlbum = function(album) {
 	albumSongList.innerHTML = '';
 	
 	// #4 for loop to go through all the songs from the specified album object and insert them ito the HTML using innerHTML property. createSongRow is called at each loop, passing song number, name, and length argument from our album object
-	for (var i = 0; i < albumSongList.length; i++) {
-		albumSongList.innterHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+	for (var i = 0; i < album.songs.length; i++) {
+		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };
 
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
-
 // add event listener to album cover, when user clicks, toggles betweent album objects
 	var albums = [albumPicasso, albumMarconi, albumKodiak];
 	var index = 1;
 	albumImage.addEventListener("click", function(event) {
 		setCurrentAlbum(albums[index]);
 		index++;
-		if (index == albums.length) {
+		if (index === albums.length) {
 			index = 0;
 		}
 	});
