62391fab37b9bed649b672ee12adb874f8aea198
Kristin Gritt <kristin.gritt@gmail.com>
2017-09-26 21:32:06 -0500

    completes checkpoint 17

--- scripts/album.js
+++ scripts/album.js
@@ -39,7 +39,7 @@ var createSongRow = function (songNumber, songName, songLength) {
       + '   <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>';
  
-    return template;
+    return $(template);
 
 };
 
@@ -47,24 +47,26 @@ var createSongRow = function (songNumber, songName, songLength) {
 
 var setCurrentAlbum = function (album) {
 	// #1 select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list. 
-	var albumTitle = document.getElementsByClassName('album-view-title')[0];
-	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-	var albumImage = document.getElementsByClassName('album-cover-art')[0];
-	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
+
 	
 	// #2 the firstChild property identifies the first child node of an element, and nodeValue returns or sets the value of a node.
-	albumTitle.firstChild.nodeValue = album.title;
-	albumArtist.firstChild.nodeValue = album.artist;
-	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-	albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
 	
 	// #3 empty the string to ensure we've got a clean slate and there are no interfering elements
-	albumSongList.innerHTML = '';
+	$albumSongList.empty();
 	
 	// #4 for loop to go through all the songs from the specified album object and insert them ito the HTML using innerHTML property. createSongRow is called at each loop, passing song number, name, and length argument from our album object
 	for (var i = 0; i < album.songs.length; i++) {
-		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
 	}
 };
 
--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,6 @@
-var collectionTemplate = // <--- variable to hold the template to create each album. When script runs it caches content.
-	'<div class="collection-album-container column fourth">' // add quotation marks to make the template a string
+var buildCollectionItemTemplate = function() { // <--- variable to hold the template to create each album. When script runs it caches content.
+	var template = 
+      '<div class="collection-album-container column fourth">' // add quotation marks to make the template a string
 	+ '	<img src="assets/images/album_covers/01.png"/>' // + used to keep strong together
 	+ '	<div class="collection-album-info caption">'
 	+ '		<p>'
@@ -12,16 +13,20 @@ var collectionTemplate = // <--- variable to hold the template to create each al
 	+ '		</p>'
 	+ '	</div>'
 	+ '</div>'
-	;
-// now that template is created, we can use the variable to add as many albums that we want
-window.onload = function() {
-	// #1 select the first - and only - element with an album-covers name. assigned to variable collectionContainer
-	var collectionContainer = document.getElementsByClassName('album-covers')[0];
-	// #2 assign an empty string to collectionContainer's innerHTML property to clear its content. Ensures we're working with a clean slate before we insert content with JavaScript
-	collectionContainer.innerHTML = '';
+;
+  
+  // wrap template in a jQuery objects to future-proof it 
+    return $(template);
+};
+
+$(window).load(function() {
+	var $collectionContainer = $('.album-covers');
+	$collectionContainer.empty();
 
 	// #3 for loop insters 12 albums using the += operator. Each loop adds the contents of collectionTemplate to the innerHTML of collectionContainer
 	for (var i = 0; i < 12; i++) {
-		collectionContainer.innerHTML += collectionTemplate;	
+		var $newThumbnail = buildCollectionItemTemplate();	
+        // #5
+        $collectionContainer.append($newThumbnail);
 	}
-}
+});
